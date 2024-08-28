ca2f0c7aaaa2249322fe870a3856942a0d634962
Jonathan Bouchet <jonathanbouchet@Jonathans-MacBook-Pro.local>
2024-08-28 10:31:30 -0700

    fix url

--- main.py
+++ main.py
@@ -16,10 +16,16 @@ app = FastAPI(lifespan=lifespan)
 async def root() -> dict[str, str]:
     return {"msg":"Hello World"}
 
+
+@app.get("/ping", tags=["ping"])
+async def ping() -> dict[str, str]:
+    return {"msg":"ping"}
+
 @aiocron.crontab('*/2 * * * *')
-@app.get("/", tags=["ping"])
-async def self_ping():
+def self_ping() -> None:
     ping_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"ping time: {ping_time}")
-    response = requests.get(url="http://127.0.0.1:8000")
-    print(f"Health check response: {response.status_code}")
+    # response = requests.get(url="http://127.0.0.1:8000/ping")
+    response = requests.get(url="https://fastapi-ping.onrender.com/ping")
+    print(f"ping code: {response.status_code}")
+    print(f"ping code: {response.content}")
